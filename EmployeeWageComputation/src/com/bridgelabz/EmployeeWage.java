340f27f82732216efec3cecc9c6cec900ccc6ef0
csumn <csumn13@gmail.com>
2022-05-06 17:23:27 +0530

    UC2 added

--- EmployeeWageComputation/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWageComputation/src/com/bridgelabz/EmployeeWage.java
@@ -1,14 +1,17 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+	private static final int WAGE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
 		int isPresent = 1;
 		int value = (int)Math.floor(Math.random()*10) % 2;
 		if(value == isPresent) {
-			System.out.println("Employee is Present");
+			int empHrs = 8;
+			int empWage = WAGE_PER_HOUR * empHrs; 
+			System.out.println("Employee is Present and Wage is :" +empWage);
 		}
 		else 
-			System.out.println("Employee is Absent");	
-
+			System.out.println("Employee is Absent ....");	
 	}
 }
